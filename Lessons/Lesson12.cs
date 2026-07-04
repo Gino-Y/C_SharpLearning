f2add8279b1073be5bc72cbc98a0b9c6f98e7aaf
RF-202508210849\Administrator <ginoyan250@gmail.com>
2026-07-04 19:31:12 +0800

    Switch

---
+++ Lessons/Lesson12.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharpLearning.Lessons;
+
+public class Lesson12
+{
+    public static void Run()
+    {
+        // 分支语句
+        int a = 9;
+
+        switch (a)
+        {
+            case 0:
+                Console.WriteLine("a == 0");
+                break;
+            case 1:
+                Console.WriteLine("a == 1");
+                break;
+            case 2:
+                Console.WriteLine("a == 2");
+                break;
+            default:
+                Console.WriteLine("a == ?");
+                break;
+        }
+    }
+}
