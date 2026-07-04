a192a4d4a901c9cc3a4b5bee928e66a6db3d9e2f
RF-202508210849\Administrator <ginoyan250@gmail.com>
2026-07-04 19:31:22 +0800

    tijiao

--- Lessons/Lesson11.cs
+++ Lessons/Lesson11.cs
@@ -7,7 +7,28 @@ namespace CSharpLearning.Lessons
         public static void Run()
         {
             // 逻辑语句
-            int a = 1;
+            int a = 6;
+            // if (a > 10)
+            // {
+            //     Console.WriteLine("a is greater than 10");
+            // }
+            // else if (a < 5)
+            // {
+            //     Console.WriteLine("a is less than 5");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("a is between 5 and 10");
+            // }
+            
+            if (a < 10 && a > 5)
+            {
+                Console.WriteLine("a is between 5 and 10");
+            }
+            else
+            {
+                Console.WriteLine("a is not between 5 and 10");
+            }
             Console.WriteLine($"a = {a}");
         }
     }
